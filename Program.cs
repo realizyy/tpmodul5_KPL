02201d42ea79b3ef6a1147e6cde71ec6721b7d1d
realizyy <realizy@student.telkomuniversity.ac.id>
2022-03-24 07:24:26 +0800

    Ver 2, menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using tpmodul5_1302204035;
+
 Console.WriteLine("Hello, World!");
+SayaTubeVideo vid1 = new SayaTubeVideo("Tutorial Design By Contract - Muhammad Real Andri Faqih", 0);
+vid1.PrintVideoDetails();
+vid1.IncreasePlayCount(100);
+vid1.PrintVideoDetails();
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,28 @@
+﻿namespace tpmodul5_1302204035;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+    
+    public SayaTubeVideo(string title, int playCount)
+    {
+        //random id
+        this.id = new System.Random().Next(999);
+        this.title = title;
+        this.playCount = playCount;
+    }
+    
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID : " + id);
+        Console.WriteLine("Title : " + title);
+        Console.WriteLine("Play Count : " + playCount);
+    }
+}
